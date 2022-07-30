61f0a0faf7adb9189c3d39b371903acf4ff4824b
DiegoEscobedo17 <diego.escobedo@tecsup.edu.pe>
2022-07-30 12:39:01 -0500

    main

--- solmin/sistema/forms.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import fields
-from django import forms
-from .models import *
-
-class OrdenForm(forms.ModelForm):
-    class Meta:
-        model = Orden
-        fields = '__all__'
